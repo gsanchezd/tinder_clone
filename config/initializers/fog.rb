362a386be25f9b24bf56c4db272ea51e11af4d40
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-12-04 22:44:17 -0600

    setting region to eu-west

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,16 +1,16 @@
-if Rails.env.test? 
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
-  end
-else
+# if Rails.env.test? 
+#   CarrierWave.configure do |config|
+#     config.storage = :file
+#     config.enable_processing = false
+#   end
+# else
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
       :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-      :aws_secret_access_key  => ENV['aws_secret_access_key']   # required
-      #:region                 => 'us-east-1'                   # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],  # required
+      :region                 => 'eu-west-1'                    # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
@@ -19,4 +19,4 @@ else
     config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
     config.fog_authenticated_url_expiration = 60000 # 1000 minutes 
   end
-end
+# eend
