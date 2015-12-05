f51b7cb42a45a57d2a27a79252dd812603d27055
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-12-04 22:28:03 -0600

    WIP on fog

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
+gem 'dotenv-rails', :groups => [:development, :test]
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -26,7 +27,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem "slim"
 gem "slim-rails"
 
+gem 'net-ssh'
 gem 'carrierwave'
+gem "fog"
+gem 'mini_magick'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/initializers/fog.rb
@@ -0,0 +1,22 @@
+if Rails.env.test? 
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                         # required
+      :aws_access_key_id      => ENV['aws_access_key_id'],      # required
+      :aws_secret_access_key  => ENV['aws_secret_access_key']   # required
+      #:region                 => 'us-east-1'                   # optional, defaults to 'us-east-1'
+      #:host                   => 's3.example.com',             # optional, defaults to nil
+      #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    config.fog_directory  = ENV['aws_dir']                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+    config.fog_authenticated_url_expiration = 60000 # 1000 minutes 
+  end
+end
