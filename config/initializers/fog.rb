4f5b2544db424f1db8e0ea9cdcc08025341a02fb
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-12-04 23:05:42 -0600

    trying with region and host

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -9,8 +9,9 @@
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
       :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-      :aws_secret_access_key  => ENV['aws_secret_access_key']   # required
-      #:region                 => 'us-east-1'                   # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],  # required
+      :region                 => 'eu-west-1',                   # optional, defaults to 'us-east-1'
+      :host                   => 's3.amazonaws.com'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
@@ -19,4 +20,4 @@
     config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
     config.fog_authenticated_url_expiration = 60000 # 1000 minutes 
   end
-# end
+# eend
